1da0b0ee9940f7a2b83447cfffa611fc354598a3
Rails project by Praveen <praveen433.ece@gmail.com>
2016-04-13 14:09:36 +0000

    update action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,20 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+   @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article udpated succesfully"
+      redirect_to article_path(@article)
+      else
+      render 'edit'
+    end
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -21,6 +35,4 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :description)
   end
-
-
 end

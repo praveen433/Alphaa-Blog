3a3be2df4e75227be04a46dd02d81e4e9d1d4c94
Rails project by Praveen <praveen433.ece@gmail.com>
2016-04-27 00:44:07 +0000

    index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+  
   def new
     @user = User.new
   end
+  
   def create
     @user = User.new(user_params)
     if @user.save

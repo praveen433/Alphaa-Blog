5046f3e629d528d3f54a4fecf6da5c621b8d7af6
Rails project by Praveen <praveen433.ece@gmail.com>
2016-04-28 09:32:56 +0000

    sign in upon singup

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -56,5 +56,6 @@ class ArticlesController < ApplicationController
     if current_user != @article.user
       flash[:danger] = "You can only edit or delete your own articles"
       redirect_to root_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
     flash[:success] = "Welcome to the alpha blog #{@user.username}"
-    redirect_to articles_path
+    redirect_to user_path(@user)
     else
     render 'new'
     end
@@ -40,7 +41,7 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-  
+
   def require_same_user
     if current_user != @user
       flash[:danger] = "you can only edit your own profile"
